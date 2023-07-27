570df5d10eebd5be57f8faa68ea329832f373fb3
dm <dmendoza@popular-safi.com>
2023-07-27 01:19:59 -0500

    casa

--- src/app/tabla-service.service.ts
+++ src/app/tabla-service.service.ts
@@ -3,14 +3,12 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class TablaService {
   private apiUrl = 'http://localhost:8085/proyecto-reportes/api/reporte-riesgo';
 
   constructor(private http: HttpClient) { }
-
 
   getTablaData(fecha?: string): Observable<any[]> {
     let url = this.apiUrl;
@@ -18,11 +16,12 @@ export class TablaService {
     if (fecha) {
       url += '/' + fecha;
     }
-    console.log(url)
+    console.log(url);
 
     // Agregar datos de autenticación básica en el encabezado de la solicitud
     const headers = new HttpHeaders({
-      'Authorization': 'Basic ' + btoa('user:541f3fbf-f78f-4987-a7a3-ae999b7bf5a7')
+      Authorization:
+        'Basic ' + btoa('user:541f3fbf-f78f-4987-a7a3-ae999b7bf5a7'),
       // Reemplaza 'username' y 'password' con tus credenciales 1e694cf7-61a9-4764-af61-26217a10eda9
     });
     return this.http.get<any[]>(url, { headers });
