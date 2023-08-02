1ca62d0a0590e99e0457d314fa340b0374f4f545
dm <dmendoza@popular-safi.com>
2023-08-02 11:06:54 -0500

    m

--- src/app/views/tabla/tabla-service.service.ts
+++ src/app/views/tabla/tabla-service.service.ts
@@ -21,7 +21,7 @@ export class TablaService {
     // Agregar datos de autenticación básica en el encabezado de la solicitud
     const headers = new HttpHeaders({
       Authorization:
-        'Basic ' + btoa('user:dd5a145b-e85a-4d3e-bb52-7aaa70d6b92a'),
+        'Basic ' + btoa('user:ad48c248-8800-42e6-8dee-c30a686706a3'),
       // Reemplaza 'username' y 'password' con tus credenciales 1e694cf7-61a9-4764-af61-26217a10eda9
     });
     return this.http.get<any[]>(url, { headers });
